f626a00ea8b8dce4264b6c2d560c2e4565dafd24
Arpad G. Bondor [Gabriel] <arpad.g.bondor@gmail.com>
2025-09-19 09:51:34 +0100

    fix: add leaf decor

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import BackgroundDecor from './components/decoration/BackgroundDecor';
 export default function App() {
   return (
-    <div className="min-h-screen w-full flex flex-col justify-center items-center relative bg-brand-purple-50 bg-no-repeat bg-cover bg-bottom">
+    <div className="min-h-screen w-full flex flex-col justify-center items-center relative bg-brand-purple-50 bg-no-repeat bg-cover bg-bottom overflow-hidden">
       <BackgroundDecor />
       <div className="relative p-8 rounded-xl border-4 border-brand-green-500 bg-brand-purple-50">
         <h1 className="text-4xl font-bold text-brand-purple-600">
-          Holiday Management System
+          Leaf Management
         </h1>
         <button className="mt-6 px-6 py-2 rounded-xl bg-brand-green-500 text-white hover:bg-brand-green-600 transition">
           Get Started
--- src/components/decoration/BackgroundDecor.tsx
+++ src/components/decoration/BackgroundDecor.tsx
@@ -1,14 +1,56 @@
+import LeafDecor from './LeafDecor';
+
 export default function BackgroundDecor() {
   return (
     <>
-      <div className="absolute z-0 w-96 h-96 rounded-full rounded-bl-none rotate-35 bg-brand-purple-400 -top-[10%] -right-[10%]"></div>
-      <div className="absolute z-0 w-96 h-96 rounded-full rounded-bl-none rotate-165 bg-brand-purple-200 -bottom-[10%] right-[10%]"></div>
-      <div className="absolute z-0 w-96 h-96 rounded-full rounded-bl-none rotate-275 bg-brand-purple-300 top-[30%] -left-[10%]"></div>
-      <div className="absolute z-0 w-32 h-32 rounded-full rounded-bl-none rotate-125 bg-brand-green-400 bottom-[50%] left-[10%]"></div>
-      <div className="absolute z-0 w-32 h-32 rounded-full rounded-bl-none rotate-325 bg-brand-green-100 bottom-[70%] left-[90%]"></div>
-      <div className="absolute z-0 w-32 h-32 rounded-full rounded-bl-none rotate-220 bg-brand-green-200 bottom-[20%] left-[60%]"></div>
-      <div className="absolute z-0 w-32 h-32 rounded-full rounded-bl-none rotate-75 bg-white bottom-[20%] left-[30%]"></div>
-      <div className="absolute z-0 w-32 h-32 rounded-full rounded-bl-none rotate-215 bg-white top-[10%] left-[45%]"></div>
+      <LeafDecor
+        size="192"
+        theme="purple-3"
+        rotation={175}
+        position="top-[30%] -left-[10%]"
+      />
+      <LeafDecor
+        size="192"
+        theme="purple-4"
+        rotation={35}
+        position="-top-[10%] -right-[10%]"
+      />
+      <LeafDecor
+        size="96"
+        theme="green-2"
+        rotation={75}
+        position="-top-[10%] left-[10%]"
+      />
+      <LeafDecor
+        size="96"
+        theme="white"
+        rotation={75}
+        position="bottom-[10%] left-[20%]"
+      />
+      <LeafDecor
+        size="96"
+        theme="purple-4"
+        rotation={165}
+        position="-bottom-[10%] right-[10%]"
+      />
+      <LeafDecor
+        size="48"
+        theme="green-2"
+        rotation={220}
+        position="bottom-[20%] left-[60%]"
+      />
+      <LeafDecor
+        size="24"
+        theme="green-1"
+        rotation={125}
+        position="bottom-[70%] left-[10%]"
+      />
+      <LeafDecor
+        size="24"
+        theme="green-2"
+        rotation={325}
+        position="bottom-[40%] left-[90%]"
+      />
     </>
   );
 }
