a6c833f266bd13d19797b0fcfe392a6046503221
Arpad G. Bondor [Gabriel] <arpad.g.bondor@gmail.com>
2025-10-19 21:27:35 +0100

    fix: set consistent design for disabled input fields

--- src/components/inputs/NumberInput.tsx
+++ src/components/inputs/NumberInput.tsx
@@ -51,7 +51,7 @@ export default function NumberInput({
         step={step}
         className={`block w-full py-2 px-4 rounded-full border focus:outline-none ring-offset-brand-purple-50 focus:ring-2 focus:ring-offset-2 ${
           disabled
-            ? 'border-gray-700 bg-gray-200 text-gray-900'
+            ? 'border-brand-purple-700 bg-brand-purple-100 text-brand-purple-700 cursor-not-allowed pointer-events-none'
             : error
             ? 'focus:ring-red-700 border-red-700 hover:border-red-600 bg-red-200 hover:bg-red-100 text-red-900'
             : 'focus:ring-brand-green-700 border-brand-green-700 hover:border-brand-green-600 bg-brand-green-200 hover:bg-brand-green-100 text-brand-purple-900'
