f3d7d1b02e4862af51535e6eb204992e41aa9f03
Arpad G. Bondor [Gabriel] <arpad.g.bondor@gmail.com>
2025-10-27 18:31:49 +0000

    fix: show user count on Manage Team nav item

--- src/context/types.ts
+++ src/context/types.ts
@@ -1,5 +1,6 @@
 // user
 export const SET_USER = 'SET_USER';
+export const SET_USER_COUNT = 'SET_USER_COUNT';
 export const SET_LOGGED_IN = 'SET_LOGGED_IN';
 export const SET_LOADING = 'SET_LOADING';
 export const SET_HAS_PASSWORD = 'SET_HAS_PASSWORD';
