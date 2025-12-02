b4acfc19ef6b0f8babcf75b1c4cd9a3428891bb9
Arpad G. Bondor [Gabriel] <arpad.g.bondor@gmail.com>
2025-12-02 20:43:12 +0000

    fix: make info bubbles collapsed by default

--- src/components/info/InfoBubble.tsx
+++ src/components/info/InfoBubble.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import { FaInfoCircle } from '../../icons/fa';
 
 interface InfoBubbleProps {
@@ -6,10 +6,18 @@ interface InfoBubbleProps {
 }
 
 export default function InfoBubble({ children }: InfoBubbleProps) {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="flex gap-2 sm:gap-4 border border-brand-green-700 bg-brand-green-50 p-2 sm:p-4 text-brand-green-800">
+    <div
+      className="flex gap-2 sm:gap-4 border border-brand-green-700 bg-brand-green-50 p-2 sm:p-4 text-brand-green-800 cursor-pointer"
+      onClick={() => setOpen(!open)}
+    >
       <FaInfoCircle className="mt-1 text-brand-green-600 flex-shrink-0" />
-      <p>{children}</p>
+
+      <div className={`${open ? 'line-clamp-none' : 'line-clamp-1'}`}>
+        {children}
+      </div>
     </div>
   );
 }
