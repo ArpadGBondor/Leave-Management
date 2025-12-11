b0c58368e9d397f7e2aa45d14000561e8a1f6587
Arpad G. Bondor [Gabriel] <arpad.g.bondor@gmail.com>
2025-12-11 21:00:45 +0000

    fix: adjust mobile design

--- src/components/pageWrapper/PageWrapper.tsx
+++ src/components/pageWrapper/PageWrapper.tsx
@@ -15,14 +15,14 @@ export default function PageWrapper({
 }: PageWrapperProps) {
   return (
     <div
-      className={`w-full h-full sm:w-auto sm:h-auto min-w-md ${
+      className={`w-full h-full sm:w-auto sm:h-auto md:min-w-md ${
         variant === 'primary'
           ? 'bg-brand-green-600 sm:bg-gradient-to-br sm:from-brand-green-600  sm:to-brand-green-800'
           : 'bg-brand-purple-600 sm:bg-gradient-to-br sm:from-brand-purple-600  sm:to-brand-purple-800'
-      }  p-0 sm:p-6 lg:p-8 sm:pt-2 lg:pt-4 m-0 sm:m-4 lg:m-8 rounded-none sm:rounded-xl flex flex-col justify-stretch items-stretch ${size} overflow-hidden`}
+      }  p-0 sm:p-4 md:p-6 lg:p-8 sm:pt-2 md:pt-2 lg:pt-4 m-0 sm:m-2 md:m-4 lg:m-8 rounded-none sm:rounded-xl flex flex-col justify-stretch items-stretch ${size} overflow-hidden`}
     >
-      <header className="mb-2 sm:mb-4">
-        <h1 className=" pl-4 sm:pl-0 text-4xl font-bold text-white  [text-shadow:_1px_1px_1px_rgba(255,255,255,0.25),_-1px_-1px_1px_rgba(0,0,0,0.5)]">
+      <header className="mb-2 md:mb-4">
+        <h1 className=" pl-4 sm:pl-0 text-2xl md:text-4xl font-bold text-white  [text-shadow:_1px_1px_1px_rgba(255,255,255,0.25),_-1px_-1px_1px_rgba(0,0,0,0.5)]">
           {title}
         </h1>
       </header>
